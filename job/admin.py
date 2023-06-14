d5635321c1338a97d751ebcc8a4d80cf9082dc5d
Mohamed Amin <mohamed.amin@banha.local>
2023-06-14 12:00:11 +0300

    add job views and urls

--- job/admin.py
+++ job/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Job
+from .models import Category
 
 admin.site.register(Job)
+admin.site.register(Category)
--- job/views.py
+++ job/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+def job_list(request):
+    pass
+
+def job_details(request, id):
+    pass
